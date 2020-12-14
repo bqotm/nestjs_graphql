6cc575a6224a08fa0b44760d5a5808140de7aad6
bothmane <baq.othmane@outlook.fr>
2020-12-14 21:06:23 +0100

    first add

---
+++ src/student/create-student.input.ts
@@ -0,0 +1,14 @@
+import { Field, InputType } from "@nestjs/graphql";
+import { MinLength } from "class-validator";
+
+@InputType()
+export class CreateStudentInput {
+
+     @MinLength(2)
+     @Field()
+     firstName: string;
+
+     @MinLength(2)
+     @Field()
+     lastName: string;
+}
